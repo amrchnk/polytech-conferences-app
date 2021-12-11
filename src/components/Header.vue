234cf87e6803be7cd409d68aaf0ab0b4a42e2c40
amirchenko <amirchenko@ozon.ru>
2021-12-11 19:16:53 +0300

    [fe] first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="navbar">
+      <router-link class="navbar-brand" to="/"><b class="header">Московский политех. Конференции</b></router-link>
+
+      <ul class="links justify-content-center">
+        <li>
+          <router-link class="nav-link" to="/">Главная</router-link>
+        </li>
+      </ul>
+      <ul class="btns">
+        <li>
+          <router-link to="/autho" type="button" class="btn btn-outline-primary">Авторизация</router-link>
+        </li>
+        <li>
+          <router-link to="/reg" type="button" class="btn btn-outline-primary">Регистрация</router-link>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {},
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: white;
+  display: flex;
+}
+
+.navbar {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+</style>
